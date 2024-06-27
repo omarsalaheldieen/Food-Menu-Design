4dc5569ffd55940725286dde398f7e9c9cddbe92
omar salah <114373468+omarsalaheldieen@users.noreply.github.com>
2024-06-27 03:10:27 +0300

    feat: Add AR

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,22 @@
 var cards = document.querySelectorAll(".card");
-
+let menu = document.querySelector(".container");
 for (let card of cards) {
   card.addEventListener("click", function () {
     var title = card.querySelector(".card-title").textContent;
 
+    // create Iframe
+    const iframeContainer = document.getElementById("iframe-container");
+    const iframe = document.getElementById("ar-frame");
+    iframe.src = "assets/tabs/" + title + ".html"; // Path to the AR app
+    iframeContainer.style.display = "block";
+    menu.style.display = "none";
     console.log("Clicked Burger:", title);
   });
+}
+
+function closeSelf() {
+  const iframeContainer = document.getElementById("iframe-container");
+  const iframe = document.getElementById("ar-frame");
+  iframeContainer.style.display = "none";
+  menu.style.display = "block";
 }
